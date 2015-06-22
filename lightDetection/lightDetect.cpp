24853fb83e5c5e7eff1da3ebdb2faa2b98208d44
Alex Gurney <thaeds@gmail.com>
2015-06-21 20:54:36 -0400

    V1 Working stoplight detection code

--- lightDetection/lightDetect.cpp
+++ lightDetection/lightDetect.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
+#define HIST_FRAMES 5     // Number of frames to keep in history
+#define LIGHT_DIST_PIX 35 // Pixels between the center of red and green lights
+#define MAXSUMRESULTSRED2GREEN 3400000 // experimental value of the max of the sum of all results pixels during red2green
+#define TRIGGERPERCENTAGE .8 // The percentage an event must be from experimental, accounting for noise, to trigger
 
+using namespace cv;
+using namespace std;
+
+// What happens when the stoplight changes from red to green?
+// The stoplight has a red light on from the start
+// This light is very bright, and can wash out the image
+// It typically looks like a reddish halo around a white circle
+// The stoplight then turns on the green light, and both are on
+// This looks like two white circles one over the other,
+// The top having a red halo, and the bottom a green halo
+// This lasts for a few frames, then the red light turns off
 int main(int argc, char** argv)
 {
     VideoCapture cap(1);
@@ -11,63 +28,59 @@ int main(int argc, char** argv)
     }
 
     for (;;) {
-      // Intialize variables
-      Mat frame, noWhite, noWhiteMask, redLight, greenLight;
-      cap >> frame;
-	
-    
-      cvtColor(frame, noWhiteMask, CV_BGR2GRAY);
-      threshold(noWhiteMask, noWhiteMask, 250, 255, THRESH_BINARY_INV);
-      frame.copyTo(noWhite, noWhiteMask);
+          // Intialize variables
+        Mat frame, noWhite, noWhiteMask, redLight, greenLight;
+        static Mat history[HIST_FRAMES];
+        static int counttostart = 1;
 
-      inRange(noWhite, Scalar(0, 80, 240), Scalar(50, 150, 255), redLight);
-      inRange(noWhite, Scalar(50, 150, 70), Scalar(150, 250, 170), greenLight);
+        cap >> frame;
 
-      int size = 11;
-      Mat element = getStructuringElement(MORPH_RECT, Size(2*size+1, 2*size+1), Point(size, size));
-
-      dilate(redLight, redLight, element);
-      dilate(greenLight, greenLight, element);
-      erode(redLight, redLight, element);
-      erode(greenLight, greenLight, element);
-
-      imshow("redLight", redLight);
-      imshow("greenLight", greenLight);
-
-
-      //grayscale and blur
-      Mat img;
-      cvtColor(frame, img, CV_BGR2GRAY);
-      Mat cimg;
-      medianBlur(img, img, 5);
-      cvtColor(img, cimg, CV_GRAY2BGR);
-
-      vector<Vec3f> circles;
-      HoughCircles(img, circles, 3, 1, 10,
-                   100, 30, 1, 30 // change the last two parameters
-                   // (min_radius & max_radius) to detect larger circles
-                   );
-
-      int redCircle = 0;
-      int greenCircle = 0;
-      for( size_t i = 0; i < circles.size(); i++ )
-        {
-          Vec3i c = circles[i];
-          redCircle = redCircle || redLight.at<uchar>(c[1], c[0]);
-          greenCircle = greenCircle || greenLight.at<uchar>(c[1], c[0]);
-          if (redLight.at<uchar>(c[1], c[0]) || greenLight.at<uchar>(c[1], c[0])) {
-            circle( frame, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, 16);
-            circle( frame, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, 16);
-          }
+        for(int i = HIST_FRAMES-1; i > 0; i--) {
+            history[i] = history[i - 1];
+        }
+        history[0] = frame;
+        if (!(counttostart == HIST_FRAMES)) {
+            counttostart++;
+            continue;
         }
 
-      if (redCircle && greenCircle)
-        {
-          std::cout << "Light Changed!" << std::endl;
-        }
+        Mat past, cur;
 
+        past = history[HIST_FRAMES - 1];
+        cur = history[0];
 
-      imshow("detected circles", frame);
+        Mat translate = (Mat_<double>(2, 3) << 1 , 0 , 0 , 0 , 1 , LIGHT_DIST_PIX);
+
+        Mat gaining = cur - past;
+        Mat losing = past - cur;
+        warpAffine(losing, losing, translate, past.size());
+
+        Mat channel[3];
+        split(gaining, channel);
+        Mat gaininggreen = channel[0] - channel[2]; // we use blue because green is more blue than green
+        split(losing, channel);
+        Mat losingred = channel[2] - channel[0]/2 - channel[1]/2;
+
+        Mat geometricmean;
+        gaininggreen.convertTo(gaininggreen, CV_16UC1, 1, 0);
+        losingred.convertTo(losingred, CV_16UC1, 1, 0);
+        geometricmean = gaininggreen.mul(losingred);
+        Mat result(gaining.size(), CV_8UC1);
+//        geometricmean.convertTo(result, CV_8UC1, 1/255.0, 0);
+//        Mat both = gaining/2 + losing/2;
+
+        double sumresult = sum(result)[0];
+        if (sumresult > TRIGGERPERCENTAGE * MAXSUMRESULTSRED2GREEN)
+            cout << "GO GO GO!!!!!!" << endl;
+        else
+            cout << "Wait for it...";
+        cout << "   " << sumresult << endl;
+
+//        imshow("losing", losing);
+//        imshow("gaining", gaining);
+//        imshow("lightchange", result);
+//        imshow("both", both);
+//        imshow("frame", frame);
 
       if (waitKey(30) >= 0) break;
     }
